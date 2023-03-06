1f3f6ac6c1b42eb55df4289d48d18f977ace3497
Nigelms <almazchic@gmail.com>
2023-03-06 11:18:56 +0300

    добавлен html страница

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+var store = NewUrlStore()
 
 func Add(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	url := r.FormValue("url")
+	if url == "" {
+		fmt.Fprint(w, AddForm)
+		return
+	}
+	key := store.Put(url)
 
+	fmt.Fprintf(w, "%s", key)
 }
 
 func Redirect(w http.ResponseWriter, r *http.Request) {
