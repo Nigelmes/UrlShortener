14b159b6dfeb9f940b5875b93a4ad8c048b6906b
Nigelms <almazchic@gmail.com>
2023-03-06 11:39:15 +0300

    генератор ключа

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -9,9 +10,12 @@ var store = NewUrlStore()
 
 func Add(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+
+	tmpl, _ := template.ParseFiles("/index/homepage.html")
+
 	url := r.FormValue("url")
 	if url == "" {
-		fmt.Fprint(w, AddForm)
+		fmt.Fprint(w, tmpl)
 		return
 	}
 	key := store.Put(url)
@@ -25,7 +29,7 @@ func Redirect(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/add", Add)
-	http.HandleFunc("/", Redirect)
+	//	http.HandleFunc("/", Redirect)
 	if err := http.ListenAndServe("0.0.0.0:80", nil); err != nil {
 		panic(err)
 	}
