bca6f25db7a136282f6dda90f239381e22ea4361
Nigelms <almazchic@gmail.com>
2023-03-06 17:01:16 +0300

    добавлен бустрап для html страницы и логотип добавлены функции add и homepage

--- main.go
+++ main.go
@@ -9,28 +9,40 @@ import (
 var store = NewUrlStore()
 
 func Add(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-
-	tmpl, _ := template.ParseFiles("/index/homepage.html")
-
 	url := r.FormValue("url")
 	if url == "" {
-		fmt.Fprint(w, tmpl)
+		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
 	key := store.Put(url)
-
+	//store.Put(url)
+	//http.Redirect(w, r, "/", http.StatusFound)
 	fmt.Fprintf(w, "%s", key)
 }
 
-func Redirect(w http.ResponseWriter, r *http.Request) {
-
+func Homepage(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./index/homepage.html")
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+	}
+	tmpl.Execute(w, nil)
 }
+
+//func Redirect(w http.ResponseWriter, r *http.Request) {
+//	key := r.URL.Path[1:]
+//	url := store.Get(key)
+//	if url == "" {
+//		http.NotFound(w, r)
+//		return
+//	}
+//	http.Redirect(w, r, url, http.StatusFound)
+//}
 
 func main() {
 	http.HandleFunc("/add", Add)
-	//	http.HandleFunc("/", Redirect)
-	if err := http.ListenAndServe("0.0.0.0:80", nil); err != nil {
+	http.HandleFunc("/", Homepage)
+	//http.HandleFunc("/", Redirect)
+	if err := http.ListenAndServe("0.0.0.0:45800", nil); err != nil {
 		panic(err)
 	}
 }
